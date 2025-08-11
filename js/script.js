9996d14c958347badaa8bc8e8402a2b461dc6cbf
Kseniya <knn0201@gmail.com>
2025-08-11 13:01:54 +0300

    First JS homework

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const myCity = 'Москва'; // неизменной название города 
+console.log(myCity);
+
+let cityWeather = '+22'; // погода, меняющаяся каждый день
+console.log(cityWeather);
+
+const myResult = (128 + 445) * 2; // result - 1146
+console.log(myResult);
